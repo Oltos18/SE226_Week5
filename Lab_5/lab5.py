f3dc8816c6569116e8d9ecb1c2afa1265cb27ce7
CanCaliskan_oltos <canc953@gmail.com>
2023-04-04 17:41:22 +0300

    5.hafta push

---
+++ Lab_5/lab5.py
@@ -0,0 +1,157 @@
+import random
+words = ["a","ability","able","about","above","accept","according","account","across","act","action","activity","actually","add","address","administration","admit","adult","affect","after","again","against","age","agency","agent","ago","agree","agreement","ahead","air","all","allow","almost","alone","along","already","also","although","always","American","among","amount","analysis","and","animal","another","answer","any","anyone","anything","appear","apply","approach","area","argue","arm","around","arrive","art","article","artist","as","ask","assume","at","attack","attention","attorney","audience","author","authority","available","avoid","away","baby","back","bad","bag","ball","bank","bar","base","be","beat","beautiful","because","become","bed","before","begin","behavior","behind","believe","benefit","best","better","between","beyond","big","bill","billion","bit","black","blood","blue","board","body","book","born","both","box","boy","break","bring","brother","budget","build","building","business","but","buy","by","call","camera","campaign","can","cancer","candidate","capital","car","card","care","career","carry","case","catch","cause","cell","center","central","century","certain","certainly","chair","challenge","chance","change","character","charge","check","child","choice","choose","church","citizen","city","civil","claim","class","clear","clearly","close","coach","cold","collection","college","color","come","commercial","common","community","company","compare","computer","concern","condition","conference","Congress","consider","consumer","contain","continue","control","cost","could","country","couple","course","court","cover","create","crime","cultural","culture","cup","current","customer","cut","dark","data","daughter","day","dead","deal","death","debate","decade","decide","decision","deep","defense","degree","Democrat","democratic","describe","design","despite","detail","determine","develop","development","die","difference","different","difficult","dinner","direction","director","discover","discuss","discussion","disease","do","doctor","dog","door","down","draw","dream","drive","drop","drug","during","each","early","east","easy","eat","economic","economy","edge","education","effect","effort","eight","either","election","else","employee","end","energy","enjoy","enough","enter","entire","environment","environmental","especially","establish","even","evening","event","ever","every","everybody","everyone","everything","evidence","exactly","example","executive","exist","expect","experience","expert","explain","eye","face","fact","factor","fail","fall","family","far","fast","father","fear","federal","feel","feeling","few","field","fight","figure","fill","film","final","finally","financial","find","fine","finger","finish","fire","firm","first","fish","five","floor","fly","focus","follow","food","foot","for","force","foreign","forget","form","former","forward","four","free","friend","from","front","full","fund","future","game","garden","gas","general","generation","get","girl","give","glass","go","goal","good","government","great","green","ground","group","grow","growth","guess","gun","guy","hair","half","hand","hang","happen","happy","hard","have","he","head","health","hear","heart","heat","heavy","help","her","here","herself","high","him","himself","his","history","hit","hold","home","hope","hospital","hot","hotel","hour","house","how","however","huge","human","hundred","husband","I","idea","identify","if","image","imagine","impact","important","improve","in","include","including","increase","indeed","indicate","individual","industry","information","inside","instead","institution","interest","interesting","international","interview","into","investment","involve","issue","it","item","its","itself","job","join","just","keep","key","kid","kill","kind","kitchen","know","knowledge","land","language","large","last","late","later","laugh","law","lawyer","lay","lead","leader","learn","least","leave","left","leg","legal","less","let","letter","level","lie","life","light","like","likely","line","list","listen","little","live","local","long","look","lose","loss","lot","love","low","machine","magazine","main","maintain","major","majority","make","man","manage","management","manager","many","market","marriage","material","matter","may","maybe","me","mean","measure","media","medical","meet","meeting","member","memory","mention","message","method","middle","might","military","million","mind","minute","miss","mission","model","modern","moment","money","month","more","morning","most","mother","mouth","move","movement","movie","Mr","Mrs","much","music","must","my","myself","name","nation","national","natural","nature","near","nearly","necessary","need","network","never","new","news","newspaper","next","nice","night","no","none","nor","north","not","note","nothing","notice","now","n't","number","occur","of","off","offer","office","officer","official","often","oh","oil","ok","old","on","once","one","only","onto","open","operation","opportunity","option","or","order","organization","other","others","our","out","outside","over","own","owner","page","pain","painting","paper","parent","part","participant","particular","particularly","partner","party","pass","past","patient","pattern","pay","peace","people","per","perform","performance","perhaps","period","person","personal","phone","physical","pick","picture","piece","place","plan","plant","play","player","PM","point","police","policy","political","politics","poor","popular","population","position","positive","possible","power","practice","prepare","present","president","pressure","pretty","prevent","price","private","probably","problem","process","produce","product","production","professional","professor","program","project","property","protect","prove","provide","public","pull","purpose","push","put","quality","question","quickly","quite","race","radio","raise","range","rate","rather","reach","read","ready","real","reality","realize","really","reason","receive","recent","recently","recognize","record","red","reduce","reflect","region","relate","relationship","religious","remain","remember","remove","report","represent","Republican","require","research","resource","respond","response","responsibility","rest","result","return","reveal","rich","right","rise","risk","road","rock","role","room","rule","run","safe","same","save","say","scene","school","science","scientist","score","sea","season","seat","second","section","security","see","seek","seem","sell","send","senior","sense","series","serious","serve","service","set","seven","several","sex","sexual","shake","share","she","shoot","short","shot","should","shoulder","show","side","sign","significant","similar","simple","simply","since","sing","single","sister","sit","site","situation","six","size","skill","skin","small","smile","so","social","society","soldier","some","somebody","someone","something","sometimes","son","song","soon","sort","sound","source","south","southern","space","speak","special","specific","speech","spend","sport","spring","staff","stage","stand","standard","star","start","state","statement","station","stay","step","still","stock","stop","store","story","strategy","street","strong","structure","student","study","stuff","style","subject","success","successful","such","suddenly","suffer","suggest","summer","support","sure","surface","system","table","take","talk","task","tax","teach","teacher","team","technology","television","tell","ten","tend","term","test","than","thank","that","the","their","them","themselves","then","theory","there","these","they","thing","think","third","this","those","though","thought","thousand","threat","three","through","throughout","throw","thus","time","to","today","together","tonight","too","top","total","tough","toward","town","trade","traditional","training","travel","treat","treatment","tree","trial","trip","trouble","true","truth","try","turn","TV","two","type","under","understand","unit","until","up","upon","us","use","usually","value","various","very","victim","view","violence","visit","voice","vote","wait","walk","wall","want","war","watch","water","way","we","weapon","wear","week","weight","well","west","western","what","whatever","when","where","whether","which","while","white","who","whole","whom","whose","why","wide","wife","will","win","wind","window","wish","with","within","without","woman","wonder","word","work","worker","world","worry","would","write","writer","wrong","yard","yeah","year","yes","yet","you","young","your","yourself"]
+entered_correct=True
+while entered_correct:
+    print("Please enter a number between 1 and 999 (1 and 999 inclusive)")
+    input_1 = int(input())
+    if input_1>=1 and input_1<=999:
+        entered_correct=False
+entered_correct_2=True
+while entered_correct_2:
+    print("Please enter a number between 1 and 999 (1 and 999 inclusive")
+    input_2 = int(input())
+    if input_2>=1 and input_2<=999:
+        entered_correct_2=False
+if input_1<input_2:
+    temp_int=input_1
+    input_1=input_2
+    input_2=temp_int
+print(words[input_2:input_1])
+print("End of question 1")
+#End of question 1
+print()
+print()
+user_number=0;
+y=""
+entered_correct_3=True
+while entered_correct_3:
+    print("Please enter a number between 3 and 7 (3 and 7 inclusive)")
+    user_number=int(input())
+    if user_number>=3 and user_number<=7:
+        entered_correct_3=False
+temp_list=[]
+for x in range(user_number):
+    x=random.randrange(1000)
+    temp_list.append(words[x])
+for x in range(user_number):
+    y+=temp_list[x]
+y=y[::-1]
+print(y)
+print("End of question 2")
+#End of question 2
+print()
+print()
+set_uninon={""}
+set_shorter={""}
+set_longer={""}
+for x in range(8):
+    user_number_2=0;
+    yx=""
+    entered_correct_4=True
+    while entered_correct_4:
+        print("Please enter a number between 3 and 7 (3 and 7 inclusive)")
+        user_number_2=int(input())
+        if user_number_2>=3 and user_number_2<=7:
+            entered_correct_4=False
+    temp_set={""}
+    for x in range(user_number_2):
+        x=random.randrange(1000)
+        temp_set.add(words[x])
+    for x in temp_set:
+        yx+=x
+    yx=yx[::-1]
+    print(yx)
+    if user_number_2<5:
+        set_shorter.add(yx)
+    else:
+        set_longer.add(yx)
+print(set_shorter)
+print(set_longer)
+set_uninon=set_shorter.union(set_longer)
+print(set_uninon)
+print("End of question 3")
+#End of question 3
+print()
+print()
+Dictionary={}
+int_2=0
+total=0
+for x in range(1,31):
+    Dictionary[x]=x*int_2
+    int_2=x;
+#end of part a of question 4
+for x in range(1,31):
+    print(str(x)+ ": " + str(Dictionary[x]))
+#end of part b of question 4
+for x in range(1,31):
+    total+=Dictionary[x]
+print("the total is " + str(total))
+#end of part c of question 4
+entered_correct_5=True;
+while entered_correct_5:
+    print("Please enter a number between 1 and 30 (1 and 30 inclusive)")
+    input_del_dic=int(input())
+    if input_del_dic>=1 and input_del_dic<=30:
+        entered_correct_5=False
+del Dictionary[input_del_dic]
+for x in range(1,31):
+    if(x==input_del_dic):
+        continue
+    print(str(x)+ ": " + str(Dictionary[x]))
+print("End of question 4")
+#end of part d of question 4
+#end of question 4
+print()
+print()
+divided = {'Tony': 41, 'Rhodey': 43, 'Nat': 35}
+we_fall = {'Steve': 39, 'Clint': 35, 'Wanda': 28}
+united_we_stand={}
+divided.update(we_fall)
+united_we_stand.update(divided)
+print("Name    " + "Age")
+for keys,values in united_we_stand.items():
+    space=""
+    space_count=8-len(keys)
+    for x in range(space_count):
+        space+=" "
+    print(keys + space +  str(values))
+#end of part a of question 5
+print()
+del united_we_stand["Nat"]
+print("Name    " + "Age")
+for keys,values in united_we_stand.items():
+    space=""
+    space_count=8-len(keys)
+    for x in range(space_count):
+        space+=" "
+    print(keys + space +  str(values))
+#end of part b of question 5
+print()
+united_we_stand = dict(sorted(united_we_stand.items()))
+print("Name    " + "Age")
+for keys,values in united_we_stand.items():
+    space=""
+    space_count=8-len(keys)
+    for x in range(space_count):
+        space+=" "
+    print(keys + space +  str(values))
+# end of part c of question 5
+print()
+loop_count=0
+max_value=0
+min_value=0
+for key,value in united_we_stand.items():
+    if loop_count==0:
+        max_value=value
+    if max_value < value:
+        max_value =value
+    loop_count+=1
+print("max value " + str(max_value))
+loop_count=0
+for key,value in united_we_stand.items():
+    if loop_count==0:
+        min_value=value
+    if min_value > value:
+        min_value =value
+    loop_count+=1
+print("min value " + str(min_value))
